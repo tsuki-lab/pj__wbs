616ceb39da83f888390cb8d506c8e46f0416eba1
touka.hanetsuki <touka.hanetuki@gmail.com>
2021-02-18 23:08:44 +0900

    [ fix ] UI

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'destyle.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,45 +1,45 @@
 <template>
   <div class="home">
-    <ul>
-      <li v-for="primaryItem in primaryItems" :key="primaryItem.id">
-        {{ primaryItem.id }}
-        <ul>
-          <li v-for="secondaryItem in primaryItem.children" :key="secondaryItem.id">
-            {{ secondaryItem.id }}
-            <ul>
-              <li v-for="tertiaryItem in secondaryItem.children" :key="tertiaryItem.id">
-                {{ tertiaryItem.id }}
-                <ul>
-                  <li v-for="quaternaryItem in tertiaryItem.children" :key="quaternaryItem.id">
-                    {{ quaternaryItem.id }}
-                  </li>
-                </ul>
-                <p>
-                  <button @click="addQuaternaryToTertiaryChild(tertiaryItem)">
-                    詳細項目追加
-                  </button>
-                </p>
-              </li>
-            </ul>
-            <p>
-              <button @click="addTertiaryToSecondaryChild(secondaryItem)">
-                小項目追加
-              </button>
-            </p>
-          </li>
-        </ul>
-        <p>
-          <button @click="addSecondaryToPrimaryChild(primaryItem)">
-            中項目追加
-          </button>
-        </p>
-      </li>
-    </ul>
     <p>
       <button @click="addPrimaryToState">
         大項目追加
       </button>
     </p>
+    <ul>
+      <li v-for="primaryItem in primaryItems" :key="primaryItem.id">
+        {{ primaryItem.id }}
+        <p>
+          <button @click="addSecondaryToPrimaryChild(primaryItem)">
+            中項目追加
+          </button>
+        </p>
+        <ul v-show="0 < primaryItem.children.length">
+          <li v-for="secondaryItem in primaryItem.children" :key="secondaryItem.id">
+            {{ secondaryItem.id }}
+            <p>
+              <button @click="addTertiaryToSecondaryChild(secondaryItem)">
+                小項目追加
+              </button>
+            </p>
+            <ul v-show="0 < secondaryItem.children.length">
+              <li v-for="tertiaryItem in secondaryItem.children" :key="tertiaryItem.id">
+                {{ tertiaryItem.id }}
+                <p>
+                  <button @click="addQuaternaryToTertiaryChild(tertiaryItem)">
+                    詳細項目追加
+                  </button>
+                </p>
+                <ul v-show="0 < tertiaryItem.children.length">
+                  <li v-for="quaternaryItem in tertiaryItem.children" :key="quaternaryItem.id">
+                    {{ quaternaryItem.id }}
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -69,8 +69,22 @@ export default defineComponent({
 </script>
 
 <style scoped>
+  .home {
+    font-size: .9rem;
+  }
   ul {
     border: 1px solid #666;
     margin: .5rem;
+  }
+  li {
+    margin-left: .4rem;
+  }
+  button {
+    padding: .2rem .5rem;
+    margin-top: .2rem;
+    margin-left: .4rem;
+    background-color: #59646b;
+    border-radius: .3rem;
+    color: #fff;
   }
 </style>
