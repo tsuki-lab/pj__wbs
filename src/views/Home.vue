98f5b50ac02055cb1ff1770e7dec31ae5fede0e8
touka.hanetsuki <touka.hanetuki@gmail.com>
2021-02-20 10:50:48 +0900

    [add] 項目名入力エリアを追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,49 +1,53 @@
 <template>
   <div class="home">
-    <p>
-      <button @click="addPrimaryToState">
-        大項目追加
-      </button>
-    </p>
     <ul>
       <li v-for="primaryItem in primaryItems" :key="primaryItem.id">
         <button @click="deletePrimaryFromState(primaryItem.id)">削除</button>
         {{ primaryItem.id }}
-        <p>
-          <button @click="addSecondaryToPrimaryChild(primaryItem)">
-            中項目追加
-          </button>
-        </p>
+        <input type="text" v-model="primaryItem.name" placeholder="大項目名" />
         <ul v-show="0 < primaryItem.children.length">
           <li v-for="secondaryItem in primaryItem.children" :key="secondaryItem.id">
             <button @click="deleteSecondaryFromPrimaryChild(primaryItem, secondaryItem.id)">削除</button>
             {{ secondaryItem.id }}
-            <p>
-              <button @click="addTertiaryToSecondaryChild(secondaryItem)">
-                小項目追加
-              </button>
-            </p>
+            <input type="text" v-model="secondaryItem.name" placeholder="中項目名" />
             <ul v-show="0 < secondaryItem.children.length">
               <li v-for="tertiaryItem in secondaryItem.children" :key="tertiaryItem.id">
                 <button @click="deleteTertiaryFromSecondaryChild(secondaryItem, tertiaryItem.id)">削除</button>
                 {{ tertiaryItem.id }}
+                <input type="text" v-model="tertiaryItem.name" placeholder="小項目名" />
+                <ul v-show="0 < tertiaryItem.children.length">
+                  <li v-for="quaternaryItem in tertiaryItem.children" :key="quaternaryItem.id">
+                    <button @click="deleteQuaternaryFromTertiaryChild(tertiaryItem, quaternaryItem.id)">削除</button>
+                    {{ quaternaryItem.id }}
+                    <input type="text" v-model="quaternaryItem.name" placeholder="詳細タスク名" />
+                  </li>
+                </ul>
                 <p>
                   <button @click="addQuaternaryToTertiaryChild(tertiaryItem)">
                     詳細項目追加
                   </button>
                 </p>
-                <ul v-show="0 < tertiaryItem.children.length">
-                  <li v-for="quaternaryItem in tertiaryItem.children" :key="quaternaryItem.id">
-                    <button @click="deleteQuaternaryFromTertiaryChild(tertiaryItem, quaternaryItem.id)">削除</button>
-                    {{ quaternaryItem.id }}
-                  </li>
-                </ul>
               </li>
             </ul>
+            <p>
+              <button @click="addTertiaryToSecondaryChild(secondaryItem)">
+                小項目追加
+              </button>
+            </p>
           </li>
         </ul>
+        <p>
+          <button @click="addSecondaryToPrimaryChild(primaryItem)">
+            中項目追加
+          </button>
+        </p>
       </li>
     </ul>
+    <p>
+      <button @click="addPrimaryToState">
+        大項目追加
+      </button>
+    </p>
   </div>
 </template>
 
@@ -101,5 +105,10 @@ export default defineComponent({
     background-color: #59646b;
     border-radius: .3rem;
     color: #fff;
+  }
+  input[type=text] {
+    border: solid 1px #666;
+    padding: .1rem .2rem;
+    border-radius: .2rem;
   }
 </style>
