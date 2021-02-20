938bb018ab1fbf96a8e4751b1f8d7a3ee664a2a5
touka.hanetsuki <touka.hanetuki@gmail.com>
2021-02-20 16:25:40 +0900

    [fix] eslintがtsファイルを認識していなかったので対応

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,9 +8,12 @@ module.exports = {
     'eslint:recommended',
     '@vue/typescript/recommended'
   ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
+    parser: '@typescript-eslint/parser',
     ecmaVersion: 2020
   },
+  plugins: ['@typescript-eslint'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
