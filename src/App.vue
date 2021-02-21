dab528adfb410b39634a6c4fa3397cc5fbfd99db
touka.hanetsuki <touka.hanetuki@gmail.com>
2021-02-21 23:30:55 +0900

    [add] add router view Settings.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
 <template>
-  <router-view/>
+  <nav>
+    <router-link :to="{ name: 'Home' }">
+      Home
+    </router-link>|
+    <router-link :to="{ name: 'Settings' }">
+      Settings
+    </router-link>
+  </nav>
+  <router-view />
 </template>
---
+++ src/views/Settings.vue
@@ -0,0 +1,44 @@
+<template>
+  <h1 class="text-4xl ml-4 font-bold">
+    Settings
+  </h1>
+  <h2 class="text-2xl ml-4 font-bold">
+    Categories
+  </h2>
+  <ul>
+    <li
+      v-for="category in categories"
+      :key="category.id"
+    >
+      <input
+        v-model="category.name"
+        type="text"
+        placeholder="種別名"
+      >
+    </li>
+  </ul>
+  <div class="flex justify-end mx-2">
+    <button @click="addCreateCategory">
+      種別を追加
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue'
+import { categoryStore } from '@/store/CategoryStore'
+
+const {
+  addCreateCategory
+} = categoryStore
+
+export default defineComponent({
+  name: 'Settings',
+  setup(){
+    return {
+      categories: computed(() => categoryStore.categories),
+      addCreateCategory
+    }
+  }
+});
+</script>
