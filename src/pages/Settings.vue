041d00c14f6f0d377f6a84a7ed8217153542aa65
touka.hanetsuki <touka.hanetuki@gmail.com>
2021-02-28 10:58:58 +0900

    [fix] Setting.vue InputLabel

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="text-4xl ml-4 font-bold">
+  <h1 class="ml-4 text-4xl font-bold">
     Settings
   </h1>
   <div class="w-3/6 px-4">
@@ -12,16 +12,14 @@
         :key="category.id"
         class="flex justify-between mb-2"
       >
-        <label>
-          種別名
-          <input
-            v-model="category.name"
-            type="text"
-            placeholder="（例）設計全般"
-          >
-        </label>
+        <InputLabel
+          v-model:value="category.name"
+          type="text"
+          placeholder="（例）設計全般"
+          label="種別名"
+        />
         <button
-          class="button-close w-6 h-6 mr-4"
+          class="w-6 h-6 mr-4 button-close"
           @click="deleteCategory(category)"
         />
       </li>
@@ -38,6 +36,7 @@
 import { defineComponent, computed } from 'vue'
 import { categoryStore } from '@/store/CategoryStore'
 import { Category } from '@/model/Category';
+import InputLabel from '@/components/atoms/InputLabel.vue'
 
 const {
   addCreateCategory,
@@ -46,6 +45,9 @@ const {
 
 export default defineComponent({
   name: 'Settings',
+  components: {
+    InputLabel
+  },
   setup(){
     return {
       categories: computed(() => categoryStore.categories),
